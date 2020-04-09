dce0aa17b761df3ff00ebd81f0e8605d1ef8ea53
The Combed Thunderclap <clappedthunder@gmail.com>
2020-04-09 17:22:42 +0100

    Restructure repo

---
+++ infra/scheduler.tf
@@ -0,0 +1,18 @@
+resource "google_cloud_scheduler_job" "generate_climate_news" {
+  name        = "generate-climate-news"
+  project     = var.analytics_project
+  region      = var.region
+  description = "generate daily climate news"
+  schedule    = "0 9 * * *"
+  time_zone   = "Europe/London"
+
+  http_target {
+    http_method = "POST"
+    uri         = "https://cloudbuild.googleapis.com/v1/projects/${var.analytics_project}/triggers/BUILD-generate-climate-news:run"
+    body        = base64encode("{\"branchName\":\"master\"}")
+
+    oauth_token {
+      service_account_email = "${var.analytics_project}@appspot.gserviceaccount.com"
+    }
+  }
+}
